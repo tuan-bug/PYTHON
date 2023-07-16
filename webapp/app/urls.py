7a7a96c32d08cdbfd11621331c925a1d4bedba08
tuan-bug <92282973+tuan-bug@users.noreply.github.com>
2023-07-16 21:20:34 +0700

    fix login and search

--- webapp/app/urls.py
+++ webapp/app/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('cart/', views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout"),
     path('update_item/', views.updateItem, name="update_item"),
-    path('login/', views.login, name="login"),
+    path('login/', views.loginPage, name="login"),
     path('register/', views.register, name="register"),
+    path('logout/', views.logoutPage, name="logout"),
+    path('search/', views.searchProduct, name="search"),
 ]
--- webapp/app/views.py
+++ webapp/app/views.py
@@ -1,52 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from .models import *
 import json
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 def getHome(request):
     products = Product.objects.all()
     if request.user.is_authenticated:
-        customer = request.user.customer
+        customer = request.user
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        user_not_login = "hidden"
+        user_login = "show"
         for item in items:
             item.total = item.product.price * item.quantity
     else:
         order = None
         items = []
-    context = {'products': products, 'items': items, 'order': order}
-    return render(request,'app/home.html', context)
+        user_not_login = "show"
+        user_login = "hidden"
+    context = {'products': products, 'items': items, 'order': order, 'user_login': user_login, 'user_not_login': user_not_login}
+    return render(request, 'app/home.html', context)
 
 
 
 def cart(request):
     if request.user.is_authenticated:
-        customer = request.user.customer
+        customer = request.user
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        user_not_login = "hidden"
+        user_login = "show"
         for item in items:
             item.total = item.product.price * item.quantity
     else:
         order = None
         items = []
-    context = {'items': items, 'order': order}
+        user_not_login = "show"
+        user_login = "hidden"
+    context = {'items': items, 'order': order, 'user_login': user_login, 'user_not_login': user_not_login}
     return render(request,'app/cart.html', context)
 
 
 
 def checkout(request):
     if request.user.is_authenticated:
-        customer = request.user.customer
+        customer = request.user
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        user_not_login = "hidden"
+        user_login = "show"
         for item in items:
             item.total = item.product.price * item.quantity
     else:
         order = None
         items = []
-    context = {'items': items, 'order': order}
+        user_not_login = "show"
+        user_login = "hidden"
+    context = {'items': items, 'order': order, 'user_login': user_login, 'user_not_login': user_not_login}
     return render(request,'app/checkout.html', context)
 
 
@@ -54,7 +67,7 @@ def updateItem(request):
     data = json.loads(request.body)
     productId = data['productId']
     action = data['action']
-    customer = request.user.customer
+    customer = request.user
     product = Product.objects.get(id = productId)
     order, created = Order.objects.get_or_create(customer=customer, complete=False)
     orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
@@ -67,21 +80,56 @@ def updateItem(request):
     orderItem.save()
     if orderItem.quantity <= 0:
         orderItem.delete()
-    return JsonResponse('added',safe=False)
+    return JsonResponse('added', safe=False)
 
 
 def register(request):
     from_register = CreateUserForm()
-
-    context = {'from': from_register}
+    user_not_login = "show"
+    user_login = "hidden"
+    context = {'from': from_register, 'user_login': user_login, 'user_not_login': user_not_login}
 
     # khi người dùng ấn nút đăng kí
     if request.method == 'POST':
         from_register = CreateUserForm(request.POST)
         if from_register.is_valid(): # kiểm tra đúng yêu cầu thì lưu cái form đó lại
             from_register.save()
+            return redirect('login')
     return render(request, "app/register.html", context)
 
-def login(request):
-    context = {}
+def loginPage(request):
+    user_not_login = "show"
+    user_login = "hidden"
+    if request.user.is_authenticated: # neu da xac thuc
+        return redirect('home')
+    if request.method == 'POST':
+        userName = request.POST.get('username')
+        passWord = request.POST.get('password')
+        user = authenticate(request, username=userName, password=passWord)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'UserName or PassWord not ddungs')
+    context = {'user_login': user_login, 'user_not_login': user_not_login}
     return render(request, "app/login.html", context)
+
+def logoutPage(request):
+    logout(request)
+    return redirect('login')
+
+
+def searchProduct(request):
+    if request.method == "POST":
+        search = request.POST["searched"]
+        keys = Product.objects.filter(name__contains=search)
+        if request.user.is_authenticated:
+            customer = request.user
+            order, created = Order.objects.get_or_create(customer=customer, complete=False)
+            items = order.orderitem_set.all()
+            for item in items:
+                item.total = item.product.price * item.quantity
+        else:
+            order = None
+            items = []
+    return render(request, "app/search.html", {"search": search, "keys": keys, 'items': items, 'order': order})
