b2e432630f70ec13073ca1125da34e928627343b
tuan-bug <92282973+tuan-bug@users.noreply.github.com>
2023-08-24 08:35:00 +0700

    gfhdghd

--- webapp/app/models.py
+++ webapp/app/models.py
@@ -83,6 +83,43 @@ class OrderItem(models.Model):
     def get_total(self):
         total = self.product.price * self.quantity
         return total
+
+
+
+class OrderTemp(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    date_order = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    transaction_id = models.CharField(max_length=200, null=True)
+    def __str__(self):
+        return str(self.id)
+    @property
+    def get_cart_item(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        # lấy hết tất cả tiền của các mặt hàng
+        total = sum([item.get_total for item in orderitems])
+        return total
+class OrderItemTemp(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    # lấy tiền của mỗi sản phẩm
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+
+
+class MyOrder(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,blank=True)
+    order_temp = models.ForeignKey(OrderTemp,on_delete=models.SET_NULL, null=True, blank=True)
+    total = models.FloatField()
 class Adress(models.Model):
     customer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     name_user = models.CharField(max_length=200, null=True)
